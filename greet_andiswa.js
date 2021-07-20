463136e031995e89ffa5062a52e0d371b4be0bc6
Andiswa <andiswasinxo@gmail.com>
2021-07-20 13:35:23 +0200

    one folder

--- greet_andiswa.js
+++ greet_andiswa.js
@@ -5,3 +5,14 @@ const greet = require('./greet');
 const styledMessage = chalk.bgGreen.black(greet('Andiswa'));
 
 console.log(styledMessage)
+
+var figlet = require('figlet');
+
+figlet('Hello Andiswa!!', function(err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    console.log(data)
+});
